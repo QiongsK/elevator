0387a12ae8c32549b501d5e39379ab78c3c5b994
QiongsK <48878314+QiongsK@users.noreply.github.com>
2021-06-16 09:40:10 +0800

    Create state.h

---
+++ state.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef STATE_H
+#define STATE_H
+
+//乘客
+#define  ELEVATER_YES 1 
+#define  ELEVATER_NO  0
+//楼层
+#define  WONDER_YES   1
+#define  WONDER_NO    0
+//电梯
+#define  FULLFILL_YES 1
+#define  VOID         0
+#define  FULLFILL_NO  2
+#define  UP           3
+#define  DOWN         4
+#define  DEFAULT      5
+
+
+#endif
